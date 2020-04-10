eea159232d49f3c5229a9388f33cc8d01e8bf114
kKondratek <k.kondratek@o2.pl>
2020-04-10 14:07:36 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SettingsComponent } from './menu/settings/settings.component';
+import { RankingsComponent } from './menu/rankings/rankings.component';
+import { StatsComponent } from './menu/stats/stats.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    RankingsComponent,
+    StatsComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule

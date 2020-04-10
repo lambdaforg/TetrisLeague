315fb53006f13813d9939d47597f164de81c947b
Gacek <agry123@wp.pl>
2020-04-10 21:09:13 +0200

    Routing

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+
+  action: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    // tutaj chodzi o to że bierzemy z takiego routa http://localhost:4200/menu?action=rankings  do zmiennej action rankings
+    this.route.queryParams.subscribe(
+      (params) => {
+        this.action = params['action'];
+      }
+    );
+  };
+
+    // metoda ktora nam wlasnie tworzy taki routing np  http://localhost:4200/menu?action=rankings
+  redirectTo(pathAction: string){
+    this.router.navigate(['menu'], {queryParams : {action: pathAction}});
+  }
+
+
+
+
+}
---
+++ src/app/model/User.ts
@@ -0,0 +1,16 @@
+// Taki mockup na chwile kiedy jeszcze nie mamy backendu.
+export class User {
+  id: number;
+  login: string;
+  password: string;
+
+  Questions: Array<Questions>
+  answerQuestion1: string;
+  answerQuestion2: string;
+
+}
+export enum Questions {
+  question1 = 'Question 1',
+  question2 = 'Question 2',
+  question3 = 'Question 3'
+}

cd09f4d1db3b45e34f9efefbc412947667183c71
Mateusz <agry123@wp.pl>
2020-05-08 00:56:45 +0200

    "Login Validation"

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 import {Questions, User} from '../model/User';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from "@angular/forms";
+import {MustMatch} from "../model/form/MustMatch";
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-register',
@@ -9,14 +20,59 @@ import {Questions, User} from '../model/User';
 })
 export class RegisterComponent implements OnInit {
 
+  user: User;
   questions = Questions;
+  formRegister: FormGroup;
+  register = { name: 'Login', password: 'Password', confirmPassword: 'Password', answer0: '', answer1: '', question1: this.questions.question1, question2: this.questions.question2};
 
-  constructor(private router: Router) { }
+
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.user = new User();
+
+    this.formRegister = this.formBuilder.group({
+      name:  [this.register.name, [Validators.required, Validators.minLength(4)]],
+      password:  [this.register.password, [Validators.required, Validators.minLength(7)]],
+      confirmPassword:  [this.register.confirmPassword, [Validators.required]],
+      answer1: [this.register.answer0, Validators.required],
+      answer2: [this.register.answer1, Validators.required],
+      question1: [this.register.question1],
+      question2: [this.register.question2]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
   }
+
+
+  get name() { return this.formRegister.get('name'); }
+  get password(){ return this.formRegister.get('password');}
+  get confirmPassword(){ return this.formRegister.get('confirmPassword');}
+  get answer1(){ return this.formRegister.get('answer1');}
+  get answer2(){ return this.formRegister.get('answer2');}
+  get f() { return this.formRegister.controls; }
 
   redirectToLogin(){
     this.router.navigate(['login']);
+  }
+  submit(){
+    this.user.login = this.name.value;
+    this.user.password = this.password.value;
+    this.user.points = [];
+    this.user.answerQuestion1 = this.answer1.value;
+    this.user.answerQuestion2 = this.answer2.value;
+    this.user.name = this.name.value;
+    this.user.Questions.push(this.formRegister.get('question1').value);
+    this.user.Questions.push(this.formRegister.get('question2').value);
+
+    this.dataService.createUser(this.user).subscribe(
+      (good) =>{
+        this.dataService.event.emit(good);
+        this.router.navigate(['/login']);
+      }
+
+    )
   }
 }

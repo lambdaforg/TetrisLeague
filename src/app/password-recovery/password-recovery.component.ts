bb68e2fc3993c8f13eb6aba47768e11bf9529e20
kKondratek <k.kondratek@o2.pl>
2020-05-07 21:01:49 +0200

    Add password recovery form

--- src/app/password-recovery/password-recovery.component.ts
+++ src/app/password-recovery/password-recovery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {Questions} from '../model/User';
 
 @Component({
   selector: 'app-password-recovery',
@@ -8,13 +9,25 @@ import {Router} from '@angular/router';
 })
 export class PasswordRecoveryComponent implements OnInit {
 
+  questions = Questions;
+  recoveryState: string;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.recoveryState = 'verification';
+  }
+
+  verifyUser() {
+    this.recoveryState = 'new-password';
   }
 
   redirectToRegister() {
     this.router.navigate(['register']);
+
   }
 
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
 }

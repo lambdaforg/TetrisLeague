f4f43dbfd7cf2b21de3c8f5a374465607da30e31
Mateusz <agry123@wp.pl>
2020-05-27 21:44:47 +0200

    "Starting working with jWt"

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+const AUTH_API = environment.restUrl + '/api/users/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    return this.http.post(AUTH_API + 'signin', {
+      login: credentials.login,
+      password: credentials.password
+    }, httpOptions);
+  }
+
+  register(user): Observable<any> {
+    return this.http.post(AUTH_API + 'signup', {
+      username: user.username,
+      login: user.login,
+      password: user.password
+    }, httpOptions);
+  }
+}
